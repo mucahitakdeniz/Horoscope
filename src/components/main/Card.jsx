879bdaf5fdd07a870de81a86a472de9844df1893
mucahit <mucahitakdeniz28@gmail.com>
2024-05-08 10:39:13 +0300

    first commit

---
+++ src/components/main/Card.jsx
@@ -0,0 +1,18 @@
+const Card = ({ title, date, image, desc }) => {
+
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
+      <div className="date">
+        <h2>{date}</h2>
+      </div>
+      <img src={image} alt="" />
+      <div className="card-over">
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+};
+export default Card;
